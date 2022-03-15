cc4938bbd647e9324323bd9330909c3f031bb858
Cecília Junqueira <cicijunqueira@hotmail.com>
2022-03-14 22:45:06 -0300

    comecei o js, mas deu errado. Deixei o código para caso alguém queira dar uma olhada e arrumada

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const nomeInput = document.querySelector("#nome");
-
-const usernameInput = document.querySelector("#username");
-
-button.addData('click', updateButton);
-
-function adicionarDados () {
+function addPessoa(nome, username) {
     
-}
+    var tabela = document.getElementById("tabelaDados"); //faz isso para chegar à tabela, para conseguir manipulá-la
+    var numDeLinhas = tb.rows.lenght //vê quantas linhas tem a tabela
+    var linha = tb.insertRow(numDeLinhas); //faz isso para inserir mais uma linha após a última já existente
 
+    var cellId = linha.insertCell(0);
+    var cellNome = linha.insertCell(1);
+    var cellUsername = linha.insertCell(2);
+
+
+    //agora, vai inserir o conteúdo do formulário nas células da tabela
+    cellId.innerHTML = numDeLinhas;
+    cellNome.innerHTML = nome;
+    cellUsername = username;
+
+
+}
